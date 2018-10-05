f74518d7f6a3d0a07a029e889d1f6d248a7c253b
unknown <sashwat.anagolum@gmail.com>
2018-10-05 17:08:28 +0530

    Qconfig file to replace broken link in Medium addition tutorial

--- Qconfig.py
+++ Qconfig.py
@@ -1,4 +1,4 @@
-APIToken = """f9be8ebe6cc0b5c9970ca5ae86acad18c1dfb3844ed12b381a458536fcbf46499d62dbb33da9a07627774441860c64ac44e76a6f27dc6f09bba7e0f2ce68e9ff"""
+APIToken = """YOUR_API_TOKEN_HERE"""
 
 config = {
     'url': "https://quantumexperience.ng.bluemix.net/api"
