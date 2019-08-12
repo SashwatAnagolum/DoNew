f1971db9aa9a49b06cb215f41b716f153a7026b0
unknown <sashwat.anagolum@gmail.com>
2019-08-12 09:41:04 +0530

    Added oracle and u gate functions

--- qbayes/qbayes.py
+++ qbayes/qbayes.py
@@ -16,6 +16,51 @@ def probToAngle(prob):
     """
     return 2*arcsin(sqrt(prob))
 
+
+def oracle(circ):
+    """
+    Implements an oracle that flips the sign of states that contain P = 1.
+    """
+    circ.cu3(pi, pi, 0, net[0], net[1])
+    circ.cu3(pi, pi, 0, net[0], net[1])    
+    return circ
+
+
+def u_gate(circ):
+    """
+    Implements the U gate that flips states about the average amplitude.
+    """
+    # Implements the quantum circuit that converts ψ -> |000...0>
+    circ.u3(-1*probToAngle(0.35), 0, 0, net[0])
+    circ.u3(-1*probToAngle(0.76), 0, 0, net[1])
+    circ.u3(-1*probToAngle(0.39), 0, 0, net[2])
+
+    # Flipping the |000...0> state using a triple controlled Z gate condtioned on P, E and H, 
+    # and applied to the ancilla
+    circ.x(net)
+    circ.cu1(pi/4, net[0], net[3])
+    circ.cx(net[0], net[1])
+    circ.cu1(-pi/4, net[1], net[3])
+    circ.cx(net[0], net[1])
+    circ.cu1(pi/4, net[1], net[3])
+    circ.cx(net[1], net[2])
+    circ.cu1(-pi/4, net[2], net[3])
+    circ.cx(net[0], net[2])
+    circ.cu1(pi/4, net[2], net[3])
+    circ.cx(net[1], net[2])
+    circ.cu1(-pi/4, net[2], net[3])
+    circ.cx(net[0], net[2])
+    circ.cu1(pi/4, net[2], net[3])
+    circ.x(net)
+
+    # Implements the quantum circuit that converts |000...0> -> ψ 
+    circ.u3(probToAngle(0.35), 0, 0, net[0])
+    circ.u3(probToAngle(0.76), 0, 0, net[1])
+    circ.u3(probToAngle(0.39), 0, 0, net[2])
+
+    return circ
+
+
 # Create one 4 qubit QuantumRegister to hold the Bayesian network and an ancilla qubit,
 # and a 3 bit ClassicalRegister to hold the sampled values
 net = QuantumRegister(4, 'qreg')
@@ -32,38 +77,12 @@ circ.u3(probToAngle(0.76), 0, 0, net[1])
 # Setting up the qubit representing H assuming that E = 0
 circ.u3(probToAngle(0.39), 0, 0, net[2])
 
-# Implementing an oracle to flip states where P = 1
-circ.cu3(pi, pi, 0, net[0], net[1])
-circ.cu3(pi, pi, 0, net[0], net[1])
-
-# Implementing the U gate, which flips states about the average
-# Implements the quantum circuit that converts ψ -> |000...0>
-circ.u3(-1*probToAngle(0.35), 0, 0, net[0])
-circ.u3(-1*probToAngle(0.76), 0, 0, net[1])
-circ.u3(-1*probToAngle(0.39), 0, 0, net[2])
-
-# Flipping the |000...0> state using a triple controlled Z gate condtioned on P, E and H, 
-# and applied to the ancilla
-circ.x(net)
-circ.cu1(pi/4, net[0], net[3])
-circ.cx(net[0], net[1])
-circ.cu1(-pi/4, net[1], net[3])
-circ.cx(net[0], net[1])
-circ.cu1(pi/4, net[1], net[3])
-circ.cx(net[1], net[2])
-circ.cu1(-pi/4, net[2], net[3])
-circ.cx(net[0], net[2])
-circ.cu1(pi/4, net[2], net[3])
-circ.cx(net[1], net[2])
-circ.cu1(-pi/4, net[2], net[3])
-circ.cx(net[0], net[2])
-circ.cu1(pi/4, net[2], net[3])
-circ.x(net)
-
-# Implements the quantum circuit that converts |000...0> -> ψ 
-circ.u3(probToAngle(0.35), 0, 0, net[0])
-circ.u3(probToAngle(0.76), 0, 0, net[1])
-circ.u3(probToAngle(0.39), 0, 0, net[2])
+# Apply oracle and U gate twice
+circ = oracle(circ)
+circ = u_gate(circ)
+circ = oracle(circ)
+circ = u_gate(circ)
+circ.x(net[0])
 
 # Measure E, and rotate H to the P(1) value in the second row of the P(H|E) table condtioned on E
 circ.measure(net[1], cl[1])
@@ -78,7 +97,7 @@ backend = Aer.get_backend('qasm_simulator')
 
 # Run job many times to get multiple samples
 samples_list = []
-n_samples = 50
+n_samples = 500
 
 for i in range(n_samples):
     job = execute(circ, backend=backend, shots=1)
