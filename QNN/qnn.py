602f085733ef173dd66bdbafb2b229cd10d5689d
unknown <sashwat.anagolum@gmail.com>
2020-08-19 17:12:54 +0530

    Fixed typos, removed comments

--- QNN/qnn.py
+++ QNN/qnn.py
@@ -10,6 +10,12 @@ import numpy as np
 
 
 def convertDataToAngles(data):
+    """
+    Takes in a normalised 4 dimensional vector and returns
+    three angles such that the encodeData function returns
+    a quantum state with the same amplitudes as the
+    vector passed in.
+    """
     prob1 = data[2] ** 2 + data[3] ** 2
     prob0 = 1 - prob1
     angle1 = 2 * np.arcsin(np.sqrt(prob1))
@@ -21,6 +27,11 @@ def convertDataToAngles(data):
 
 
 def encodeData(qc, qreg, angles):
+    """
+    Given a quantum register belonging to a quantum
+    circuit, performs a series of rotations and controlled
+    rotations characterized by the angles parameter.
+    """
     qc.ry(angles[0], qreg[1])
     qc.cry(angles[1], qreg[1], qreg[0])
     qc.x(qreg[1])
@@ -29,25 +40,37 @@ def encodeData(qc, qreg, angles):
 
 
 def GGate(qc, qreg, params):
-    u00 = np.exp((0 + 1j) * params[1]) * np.cos(params[0])
-    u01 = np.exp((0 + 1j) * params[2]) * np.sin(params[0])
-    u10 = np.exp((0 + 1j) * -params[2]) * -np.sin(params[0])
-    u11 = np.exp((0 + 1j) * -params[1]) * np.cos(params[0])
-    gateLabel = "G({},{}, {})".format(
-        params[0], params[1], params[2]
+    """
+    Given a parameter α, return a single
+    qubit gate of the form
+    [cos(α), sin(α)]
+    [-sin(α), cos(α)]
+    """
+    u00 = np.cos(params[0])
+    u01 = np.sin(params[0])
+    gateLabel = "G({})".format(
+        params[0]
     )
     GGate = UnitaryGate(np.array(
-        [[u00, u01], [u10, u11]]
+        [[u00, u01], [-u01, u00]]
     ), label=gateLabel)
     return GGate
 
 
 def GLayer(qc, qreg, params):
+    """
+    Applies a layer of UGates onto the qubits of register
+    qreg in circuit qc, parametrized by angles params.
+    """
     for i in range(2):
         qc.append(GGate(qc, qreg, params[i]), [qreg[i]])
 
 
 def CGLayer(qc, qreg, anc, params):
+    """
+    Applies a controlled layer of UGates, all conditioned
+    on the first qubit of the anc register.
+    """
     for i in range(2):
         qc.append(GGate(
             qc, qreg, params[i]
@@ -55,6 +78,11 @@ def CGLayer(qc, qreg, anc, params):
 
 
 def CXLayer(qc, qreg, order):
+    """
+    Applies a layer of CX gates onto the qubits of register
+    qreg in circuit qc, with the order of application
+    determined by the value of the order parameter.
+    """
     if order:
         qc.cx(qreg[0], qreg[1])
     else:
@@ -62,6 +90,12 @@ def CXLayer(qc, qreg, order):
 
 
 def CCXLayer(qc, qreg, anc, order):
+    """
+    Applies a layer of Toffoli gates with the first
+    control qubit always being the first qubit of the anc
+    register, and the second depending on the value
+    passed into the order parameter.
+    """
     if order:
         qc.ccx(anc[0], qreg[0], qreg[1])
     else:
@@ -69,32 +103,53 @@ def CCXLayer(qc, qreg, anc, order):
 
 
 def generateU(qc, qreg, params):
+    """
+    Applies the unitary U(θ) to qreg by composing multiple
+    U layers and CX layers. The unitary is parametrized by
+    the array passed into params.
+    """
     for i in range(params.shape[0]):
         GLayer(qc, qreg, params[i])
         CXLayer(qc, qreg, i % 2)
 
 
 def generateCU(qc, qreg, anc, params):
+    """
+    Applies a controlled version of the unitary U(θ),
+    conditioned on the first qubit of register anc.
+    """
     for i in range(params.shape[0]):
         CGLayer(qc, qreg, anc, params[i])
         CCXLayer(qc, qreg, anc, i % 2)
 
 
 def getPrediction(qc, qreg, creg, backend):
+    """
+    Returns the probability of measuring the last qubit
+    in register qreg as in the |1⟩ state.
+    """
     qc.measure(qreg[0], creg[0])
-    job = execute(qc, backend=backend, shots=100000)
+    job = execute(qc, backend=backend, shots=1000)
     results = job.result().get_counts()
     if '1' in results.keys():
-        return results['1'] / 100000
+        return results['1'] / 1000
     else:
         return 0
 
 
 def convertToClass(predictions):
+    """
+    Given a set of network outputs, returns class predictions
+    by thresholding them.
+    """
     return (predictions >= 0.5) * 1
 
 
 def cost(labels, predictions):
+    """
+    Returns the sum of quadratic losses over the set
+    (labels, predictions).
+    """
     loss = 0
     for label, pred in zip(labels, predictions):
         loss += (pred - label) ** 2
@@ -103,6 +158,10 @@ def cost(labels, predictions):
 
 
 def accuracy(labels, predictions):
+    """
+    Returns the percentage of correct predictions in the
+    set (labels, predictions).
+    """
     acc = 0
     for label, pred in zip(labels, predictions):
         if label == pred:
@@ -112,6 +171,12 @@ def accuracy(labels, predictions):
 
 
 def forwardPass(params, bias, angles, backend):
+    """
+    Given a parameter set params, input data in the form
+    of angles, a bias, and a backend, performs a full
+    forward pass on the network and returns the network
+    output.
+    """
     qreg = QuantumRegister(2)
     anc = QuantumRegister(1)
     creg = ClassicalRegister(1)
@@ -123,6 +188,12 @@ def forwardPass(params, bias, angles, backend):
 
 
 def computeRealExpectation(params1, params2, angles, backend):
+    """
+    Computes the real part of the inner product of the
+    quantum states produced by acting with U(θ)
+    characterised by two sets of parameters, params1 and
+    params2.
+    """
     qreg = QuantumRegister(2)
     anc = QuantumRegister(1)
     creg = ClassicalRegister(1)
@@ -140,6 +211,12 @@ def computeRealExpectation(params1, params2, angles, backend):
 
 
 def computeGradient(params, angles, label, bias, backend):
+    """
+    Given network parameters params, a bias bias, input data
+    angles, and a backend, returns a gradient array holding
+    partials with respect to every parameter in the array
+    params.
+    """
     prob = forwardPass(params, bias, angles, backend)
     gradients = np.zeros_like(params)
     for i in range(params.shape[0]):
@@ -150,31 +227,15 @@ def computeGradient(params, angles, label, bias, backend):
                 params, newParams, angles, backend
             )
             newParams[i, j, 0] -= np.pi / 2
-            newParams[i, j, 2] = 0
-            newParams[i, j, 1] += np.pi / 2
-            prob1 = computeRealExpectation(
-                params, newParams, angles, backend
-            )
-            newParams[i, j, 2] = np.pi
-            prob2 = computeRealExpectation(
-                params, newParams, angles, backend
-            )
-            gradients[i, j, 1] = 0.5 * (prob1 + prob2)
-            newParams[i, j, 2] = params[i, j, 2] + np.pi / 2
-            newParams[i, j, 1] = 0
-            prob1 = computeRealExpectation(
-                params, newParams, angles, backend
-            )
-            newParams[i, j, 1] = np.pi
-            prob2 = computeRealExpectation(
-                params, newParams, angles, backend
-            )
-            gradients[i, j, 2] = 0.5 * (prob1 + prob2)
     biasGrad = (prob + bias - label)
     return gradients * biasGrad, biasGrad
 
 
 def updateParams(params, prevParams, grads, learningRate, momentum):
+    """
+    Updates the network parameters using gradient descent
+    and momentum.
+    """
     delta = params - prevParams
     paramsNew = np.copy(params)
     paramsNew = params - grads * learningRate + momentum * delta
@@ -182,7 +243,12 @@ def updateParams(params, prevParams, grads, learningRate, momentum):
 
 
 def trainNetwork(data, labels, backend):
-    np.random.seed(0)
+    """
+    Train a quantum neural network on inputs data and
+    labels, using backend backend. Returns the parameters
+    learned.
+    """
+    np.random.seed(1)
     numSamples = labels.shape[0]
     numTrain = int(numSamples * 0.75)
     ordering = np.random.permutation(range(numSamples))
@@ -190,22 +256,21 @@ def trainNetwork(data, labels, backend):
     validationData = data[ordering[numTrain:]]
     trainingLabels = labels[ordering[:numTrain]]
     validationLabels = labels[ordering[numTrain:]]
-    params = np.sqrt(np.random.sample((5, 2, 3)))
-
+    params = np.random.sample((5, 2, 1))
     bias = 0.01
     prevParams = np.copy(params)
     prevBias = bias
     batchSize = 5
     momentum = 0.9
     learningRate = 0.02
+
     for iteration in range(15):
         samplePos = iteration * batchSize
-        batchTrainingData = trainingData[samplePos:samplePos + 5]
-        batchLabels = trainingLabels[samplePos:samplePos + 5]
+        batchTrainingData = trainingData[samplePos:samplePos + batchSize]
+        batchLabels = trainingLabels[samplePos:samplePos + batchSize]
         batchGrads = np.zeros_like(params)
         batchBiasGrad = 0
         for i in range(batchSize):
-            # probPass=forwardPass(params, bias, batchTrainingData[i], backend)
             grads, biasGrad = computeGradient(
                 params, batchTrainingData[i], batchLabels[i], bias, backend
             )
@@ -222,7 +287,7 @@ def trainNetwork(data, labels, backend):
 
         trainingPreds = np.array([forwardPass(
             params, bias, angles, backend
-        )for angles in trainingData])
+        ) for angles in trainingData])
         print('Iteration {} | Loss: {}'.format(
             iteration + 1, cost(trainingLabels, trainingPreds)
         ))
@@ -234,7 +299,6 @@ def trainNetwork(data, labels, backend):
     )
     validationClasses = convertToClass(validationProbs)
     validationAcc = accuracy(validationLabels, validationClasses)
-    print('After {} iterations:'.format(iteration + 1))
     print('Validation accuracy:', validationAcc)
     for x, y, p in zip(validationData, validationLabels, validationClasses):
         print('Data:', x, ' | Class:', y, ' | Prediction:', p)
